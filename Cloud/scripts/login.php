70aa3640d0e704b8e3bb734cdbad83db199835a3
Jirka <D20175@oaopava.cz>
2023-03-18 17:48:51 +0100

    Druhý commit

--- Cloud/scripts/login.php
+++ Cloud/scripts/login.php
@@ -6,6 +6,7 @@ if ($_POST["username"] === "myusername" && $_POST["password"] === "mypassword") 
   $_SESSION["username"] = $_POST["username"];
   http_response_code(200);
 } else {
+  alert("Wrong username or password. Try again");
   http_response_code(401);
 }
 
